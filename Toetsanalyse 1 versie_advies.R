de7f2df7b8ea39410a4d609039e5c60d57f0ebac
Drittij <ddj200@VU.LOCAL>
2018-03-06 15:11:15 +0100

    Start met eerste toetsanalyse scripts in bitbucket te plaatsen

---
+++ Toetsanalyse 1 versie_advies.R
@@ -0,0 +1,191 @@
+################################################################
+####Dit script werkt alleen goed met tentamens met 1 versie####
+###############################################################
+
+##defineer aantal columns
+nrc <- nrq+2
+
+##Bepaal gokkans
+gk <- 1/nra
+
+##Maak ruwe data file: letter data + sleutel
+teleformdata_new <- teleformdata[ c(1:nrc) ]
+
+# ##Verwijder vragen uit dataset (optioneel te gebruiken)
+# teleformdata_new <- select(teleformdata_new, -V42, -V52)
+# nrq <- 50
+# nrc <- nrq+2
+
+##Defineer vraagnamen aanwezige vragen
+vrn <- colnames(teleformdata_new[3:nrc])
+
+##Extraheer sleutel
+sleutel <- teleformdata_new[1,3:nrc]
+write.csv2(sleutel, file=paste0(Network_directory,"sleutel.csv"), row.names=FALSE)
+
+##Verwijder eerste rij (=sleutel) uit data
+teleformdata_new <- teleformdata_new[-1, , drop=F]
+
+##Extraheer studentnummers
+studentnummers <- as.data.frame(teleformdata_new$stud_nr)
+colnames(studentnummers) <- "studentnummers"
+  
+####Extraheer data en verwijder eerste twee kolommen (=studentnamen en studentnummers)
+data <- dplyr:: select(teleformdata_new, -stud_nr, -stud_naam)
+write.csv2(data, file=paste0(Network_directory,"data.csv"), row.names=FALSE)
+
+##########################################################
+## Upload eventueel aangepaste/nieuwe sleutel
+## Om meerdere antwoorden goed te rekenen lever komma gescheiden aan
+sleutel <- read.csv2(paste0(Network_directory,"sleutel.csv"))
+
+##Transformeren van ruwe letter_data naar score data + basale analyse
+# scored_data <- score_daniel(data, sleutel, multiKeySep = ",", output.scored = TRUE, rel = TRUE)
+
+scored_data <- score(data, sleutel, output.scored = TRUE, rel = TRUE)
+
+##Toevoegen studentnummers aan score data - datax
+scored_datax <- cbind(studentnummers, scored_data$scored)
+
+##Toevoegen studentnummers aan totaalscore student
+total_score <- cbind(studentnummers, scored_data$score)
+
+##Verander column naam van totaalscore per student
+names(total_score)[names(total_score) == "scored_data$score"] <- "totaal_score"
+
+##Transformeer scores naar cijfers
+total_score <- mutate(total_score, cijfer = (10-(nrq-total_score$totaal_score)/(nrq-cesuur)*(10-5.5)))
+
+for ( i in 1:nrq ) if(total_score$cijfer[i] < 1){
+  total_score$cijfer[i] <- 1
+}
+
+##Extraheer studentnamen en schrijf weg naar csv
+studentnamen <- as.data.frame(teleformdata_new$stud_naam)
+colnames(studentnamen) <- "studentnamen"
+
+##Toevoegen studentnamen aan totaalscore student
+total_score <- cbind(studentnamen, total_score)
+
+##Wegschrijven score per student naar csv file
+write.csv2(total_score, file="results_student.csv", row.names=FALSE)
+
+## Toon cronbachs alpha
+KR20 <- scored_data$reliability$alpha
+
+##Bereken KR-20 (75)
+ifactor <- 75/nrq
+KR20_75 <- spearman.brown(KR20, input = ifactor, n.or.r = "n")
+KR20_75 <- KR20_75$r.new
+
+##Item characteristic curves (ICC) voor alle items op 1 pagina (verwijder eerste 2 regels script om losse plots te creeren)
+par(mfrow=c(4,5)) 
+par(cex = 0.4)
+for ( i in 1:nrq ) cttICC(scored_data$score, scored_data$scored[,i], colTheme="spartans", cex=1.5, ylab=names(sleutel[i]))
+
+##Functie for item distractor analysis
+distractor.analysis(data, sleutel,p.table=FALSE,write.csv="awaarden2.csv")
+awaarden <- read.csv("awaarden2.csv")
+
+names(awaarden)[names(awaarden) == "X."] <- "Lower group"
+names(awaarden)[names(awaarden) == "X..1"] <- "Middle group"
+names(awaarden)[names(awaarden) == "X..2"] <- "Upper group"
+
+write.csv2(awaarden, row.names = F, file="analyse awaarden.csv")
+
+
+##Maak itemanalyse
+pwaarde <- as.data.frame(scored_data$reliability$itemMean)
+rir <- as.data.frame(scored_data$reliability$pBis)
+newalpha <- as.data.frame(scored_data$reliability$alphaIfDeleted)
+
+itemanalyse <- cbind(pwaarde, rir, newalpha)
+names(itemanalyse)[names(itemanalyse) == "scored_data$reliability$itemMean"] <- "P_waarde"
+names(itemanalyse)[names(itemanalyse) == "scored_data$reliability$pBis"] <- "rir"
+names(itemanalyse)[names(itemanalyse) == "scored_data$reliability$alphaIfDeleted"] <- "New Alpha"
+
+##NA vervangen met nullen
+itemanalyse[is.na(itemanalyse)] <- 0
+
+##Voeg P' column toe aan itemanalyse
+itemanalyse["Rel_P"] <- NA
+
+##Bereken relatieve p-waarde
+for ( i in 1:nrq ) itemanalyse$Rel_P[i] <- ((-1/(gk-1))*itemanalyse$P_waarde[i]+1-(-1/(gk-1)))
+
+##Toetswaarden  wegschrijven
+toets <- as.data.frame(scored_data$reliability[1:5])
+toets <- tbl_df(toets)
+toets <- mutate(toets, KR20_75 = KR20_75)
+toets <- toets[,c(1,2,3,6,4,5)]
+mrelp <- summarise(itemanalyse, mean(Rel_P))
+mp <- summarise(itemanalyse, mean(P_waarde))
+toets <- mutate(toets, meanRelP = as.numeric(mrelp), meanP = as.numeric(mp))
+geslaagd <- filter(total_score, cijfer >= 5.5) %>% nrow()
+pgeslaagd <- geslaagd/nrow(total_score)
+toets <- mutate(toets, perc_geslaagd = pgeslaagd)
+toets <- mutate(toets, cesuur = as.numeric(cesuur))
+
+##Berekenen kappa
+kappa <- ((KR20)*(toets$scaleSD^2)+(toets$scaleMean-cesuur)^2)/((toets$scaleSD^2) + (toets$scaleMean-cesuur)^2)
+toets <- mutate(toets, kappa = as.numeric(kappa))
+write.csv2(toets, file="toetswaarden.csv")
+
+##Bepaal aantal studenten
+nrst <- toets$nPerson
+
+##Toevoegen A-waarde aan itemanalyse
+itemanalyse["A"] <- NA
+itemanalyse["B"] <- NA
+itemanalyse["C"] <- NA
+itemanalyse["D"] <- NA
+# itemanalyse["E"] <- NA
+
+library(stringr)
+for ( i in 1:nrq ) itemanalyse$A[i] <- (sum(str_count(data[,i], "A"))/nrst)
+for ( i in 1:nrq ) itemanalyse$B[i] <- (sum(str_count(data[,i], "B"))/nrst)
+for ( i in 1:nrq ) itemanalyse$C[i] <- (sum(str_count(data[,i], "C"))/nrst)
+for ( i in 1:nrq ) itemanalyse$D[i] <- (sum(str_count(data[,i], "D"))/nrst)
+# for ( i in 1:nrq ) itemanalyse$E[i] <- (sum(str_count(data[,i], "E"))/nrst)
+
+##Voeg advies column toe aan itemanalyse
+itemanalyse[".A"] <- NA
+itemanalyse[".B"] <- NA
+itemanalyse[".C"] <- NA
+itemanalyse[".D"] <- NA
+itemanalyse[".E"] <- NA
+
+##Genereer advies op basis van P- en rirwaarden
+for ( i in 1:nrq ) if( (itemanalyse$Rel_P[i] + itemanalyse$rir[i] < 0.4) ){
+  itemanalyse$.E[i] <- "E"
+}
+
+for ( i in 1:nrq ) if( (itemanalyse$P_waarde[i] < (gk+0.04))&(itemanalyse$rir[i] > 0.05) ){
+  itemanalyse$.D[i] <- "D"
+}
+
+for ( i in 1:nrq ) if( (itemanalyse$P_waarde[i] < 0.3)&((itemanalyse$rir[i] <= 0.05)&(itemanalyse$rir[i] >= -0.05)) ){
+  itemanalyse$.C[i] <- "C"
+}
+
+for ( i in 1:nrq ) if( (itemanalyse$Rel_P[i] < 0.4)&(itemanalyse$rir[i] <= 0.10) ){
+  itemanalyse$.A[i] <- "A"
+}
+
+for ( i in 1:nrq ) if( (itemanalyse$Rel_P[i] < 0.8)&(itemanalyse$rir[i] < -0.10) ){
+  itemanalyse$.B[i] <- "B"
+}
+
+##Verander kolom volgorde itemanalyse
+itemanalyse <- itemanalyse[,c(5,6,7,8,1,4,2,3,9,10,11,12,13)]
+
+##Verwijder NA's uit itemanalyse
+itemanalyse[,9:13] <- sapply(itemanalyse[,9:13], as.character)
+itemanalyse[,9:13][is.na(itemanalyse[,9:13])] <- " "
+
+##Schrijf itemanalyse weg naar csv
+write.csv2(itemanalyse, row.names = vrn , file="itemanalyse.csv")
+
+#Toon kappa
+kappa
+
---
+++ Voorbereidingen.R
@@ -0,0 +1,135 @@
+################################################################################
+### Voorbereidingen.R
+################################################################################
+### R code voor Tentamenanalyse Vrije Universiteit Amsterdam
+###
+### Bestandsnaam: Voorbereidingen.R
+### Doel: De working directory wordt bepaald door de locatie van het project 
+### (vu-toetsanalyse)
+### De specifieke functies en libraries voor dit project worden ingeladen
+###
+### Afhankelijkheden: geen
+###
+### Gebruikte datasets: geen
+###
+### Opmerkingen: geen
+### 
+################################################################################
+### TODO:
+### 1) Geen
+###
+################################################################################    
+### Geschiedenis:
+### 06-03-2018: DD: Aanmaken bestand
+################################################################################
+
+## installeren benodigde packages
+if(!require(CTT)){install.packages("CTT")}
+if(!require(stringr)){install.packages("stringr")}
+if(!require(dplyr)){install.packages("dplyr")}
+if(!require(psych)){install.packages("psych")}
+if(!require(schoRsch)){install.packages("schoRsch")}
+if(!require(svDialogs)){install.packages("svDialogs")}
+if(!require(ggplot2)){install.packages("ggplot2")}
+if(!require(jmv)){install.packages("jmv")}
+if(!require(readxl)){install.packages("readxl")}
+if(!require(purrr)){install.packages("purrr")}
+if(!require(knitr)){install.packages("knitr")}
+if(!require(xlsx)){install.packages("xlsx")}
+
+## laden libraries
+library(stringr)
+library(dplyr)
+library(psych)
+library(schoRsch) 
+library(svDialogs)
+library(ggplot2)
+library(jmv)
+library(readxl)
+library(purrr)
+library(knitr)
+library(xlsx)
+library(CTT)
+
+# Bepaal de netwerk directory op basis van het besturingsssyteem: windows = VU
+Network_directory_WIN <- "G:/DSZ/OKZ/OTIR/Toetsen/Werkmap/"
+Network_directory_MAC <- "/Volumes/groups/DSZ/OKZ/OTIR/Toetsen/Werkmap/"
+
+if (.Platform$OS.type == "windows") {
+  Network_directory <- Network_directory_WIN
+} else {
+  Network_directory <- Network_directory_MAC 
+}
+
+Network_directory
+
+
+## Functie om vragen na te kijken (met meerdere antwoorden goed)
+score_daniel <- function (items, key, output.scored = TRUE, ID = NA, rel = TRUE, 
+                          multiKeySep = "none", multiKeyScore = c("or", "dich")) 
+{
+  t <- as.vector(ID)
+  t <- table(ID)
+  if (any(t > 1)) {
+    for (i in 1:length(ID)) {
+      for (j in 1:nrow(subset(t, t > 1))) {
+        if (ID[i] == (rownames(subset(t, t > 1)))[j]) {
+          ID[i] <- paste(ID[i], "/", i)
+        }
+      }
+    }
+    warning("Duplicate ID exists; the duplicate ID has been renamed and retained in the calculation")
+  }
+  if (!missing(ID)) {
+    if (length(ID) == nrow(items)) 
+      rownames(items) <- ID
+    else warning("The length of ID vector does not match the sample size.")
+  }
+  if (missing(key)) {
+    warning("No key provided, assuming pre-scored data.")
+    scored <- apply(items, 2, function(XXX) {
+      if (!is.numeric(XXX)) 
+        XXX <- as.numeric(XXX)
+      XXX
+    })
+  }
+  else {
+    if (length(key) == ncol(items)) {
+      if (multiKeySep == "none") {
+        scored <- t(apply(items, 1, function(X) {
+          ifelse(X == (key), 1, 0)
+        }))
+      }
+      else {
+        scored <- array(0, dim = dim(items))
+        key <- purrr:: map_df(key, as.character)
+        items <- purrr:: map_df(items, as.character) %>% as.data.frame()
+        for (colcol in 1:ncol(items)) {
+          thisKey <- strsplit(key[[colcol]], multiKeySep)[[1]]
+          thisAnswer <- strsplit(items[, colcol], multiKeySep)
+          thisScore <- lapply(thisAnswer, function(XXX, 
+                                                   myKey = thisKey) {
+            compare <- XXX %in% myKey
+            oot <- all(c(compare, compare)) * 1
+            oot
+          })
+          scored[, colcol] <- unlist(thisScore)
+        }
+      }
+    }
+    else stop("Number of items is not equal to the length of key.")
+  }
+  scores <- rowSums(scored)
+  names(scores) <- paste("P", c(seq(1:nrow(items))), sep = "")
+  if (!rel == FALSE) 
+    reli <- reliability(scored)
+  if (output.scored == FALSE & rel == FALSE) 
+    out <- list(score = scores)
+  if (output.scored == FALSE & rel == TRUE) 
+    out <- list(score = scores, reliability = reli)
+  if (output.scored == TRUE & rel == FALSE) 
+    out <- list(score = scores, scored = scored)
+  if (output.scored == TRUE & rel == TRUE) 
+    out <- list(score = scores, reliability = reli, scored = scored)
+  out
+}
