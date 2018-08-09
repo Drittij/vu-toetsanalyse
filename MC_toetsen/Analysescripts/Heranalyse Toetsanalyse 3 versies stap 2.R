71bb8be09977876691f9a3b3e31038ab07cff72d
Daniel Drittij <d.drittij@vu.nl>
2018-08-09 12:18:02 +0200

    Aantal for loops (berekenen relatieve p waarde en adviezen) vervangen door dplyr mutate

--- MC_toetsen/Analysescripts/Heranalyse Toetsanalyse 3 versies stap 2.R
+++ MC_toetsen/Analysescripts/Heranalyse Toetsanalyse 3 versies stap 2.R
@@ -69,11 +69,9 @@ itemanalyse <- itemAnalysis(as.data.frame(scored_data$scored), NA.Delete=FALSE)$
 ##NA vervangen met nullen
 itemanalyse[is.na(itemanalyse)] <- 0
 
-##Voeg P' column toe aan itemanalyse
-itemanalyse["Rel_P"] <- NA
-
 ##Bereken relatieve p-waarde
-for ( i in 1:nrq ) itemanalyse$Rel_P[i] <- ((-1/(gk-1))*itemanalyse$P_waarde[i]+1-(-1/(gk-1)))
+itemanalyse <- itemanalyse %>% 
+  mutate(Rel_P = ((-1/(gk-1))*P_waarde+1-(-1/(gk-1))))
 
 ##Toetswaarden  wegschrijven
 geslaagd <- filter(total_score, cijfer >= 5.5) %>% nrow()
@@ -141,34 +139,13 @@ if (nra >= 6) {
   for ( i in 1:nrq ) itemanalyse$'F'[i] <- (sum(str_count(data[,i], "F"))/nrst)
 }
 
-
-##Voeg advies column toe aan itemanalyse
-itemanalyse[".A"] <- NA
-itemanalyse[".B"] <- NA
-itemanalyse[".C"] <- NA
-itemanalyse[".D"] <- NA
-itemanalyse[".E"] <- NA
-
 ##Genereer advies op basis van P- en rirwaarden
-for ( i in 1:nrq ) if( (itemanalyse$Rel_P[i] + itemanalyse$rir[i] < 0.4) ){
-  itemanalyse$.E[i] <- "E"
-}
-
-for ( i in 1:nrq ) if( (itemanalyse$P_waarde[i] < (gk+0.04))&(itemanalyse$rir[i] > 0.05) ){
-  itemanalyse$.D[i] <- "D"
-}
-
-for ( i in 1:nrq ) if( (itemanalyse$P_waarde[i] < 0.3)&((itemanalyse$rir[i] <= 0.05)&(itemanalyse$rir[i] >= -0.05)) ){
-  itemanalyse$.C[i] <- "C"
-}
-
-for ( i in 1:nrq ) if( (itemanalyse$Rel_P[i] < 0.4)&(itemanalyse$rir[i] <= 0.10) ){
-  itemanalyse$.A[i] <- "A"
-}
-
-for ( i in 1:nrq ) if( (itemanalyse$Rel_P[i] < 0.8)&(itemanalyse$rir[i] < -0.10) ){
-  itemanalyse$.B[i] <- "B"
-}
+itemanalyse <- itemanalyse %>% 
+  mutate(.A = if_else(Rel_P < 0.4 & rir <= 0.10, "A", ""), 
+         .B = if_else(Rel_P < 0.8 & rir < -0.10, "B", ""),
+         .C = if_else(P_waarde < 0.3 & rir <= 0.05 & rir >= -0.05, "C", ""),
+         .D = if_else(P_waarde < (gk+0.04) & rir > 0.05, "D", ""),
+         .E = if_else(Rel_P + rir < 0.4, "E", ""))
 
 ##Verander kolom volgorde itemanalyse
 if (nra == 2) {
@@ -196,32 +173,6 @@ if (nra == 6) {
                                                `New Alpha`, .A, .B, .C, .D, .E)
 }
 
-##Verwijder NA's uit itemanalyse
-if (nra == 2) {
-  itemanalyse[,8:12] <- sapply(itemanalyse[,8:12], as.character)
-  itemanalyse[,8:12][is.na(itemanalyse[,8:12])] <- " "
-}
-
-if (nra == 3) {
-  itemanalyse[,9:13] <- sapply(itemanalyse[,9:13], as.character)
-  itemanalyse[,9:13][is.na(itemanalyse[,9:13])] <- " "
-}
-
-if (nra == 4) {
-  itemanalyse[,10:14] <- sapply(itemanalyse[,10:14], as.character)
-  itemanalyse[,10:14][is.na(itemanalyse[,10:14])] <- " "
-}
-
-if (nra == 5) {
-  itemanalyse[,11:15] <- sapply(itemanalyse[,11:15], as.character)
-  itemanalyse[,11:15][is.na(itemanalyse[,11:15])] <- " "
-}
-
-
-if (nra == 6) {
-  itemanalyse[,12:16] <- sapply(itemanalyse[,12:16], as.character)
-  itemanalyse[,12:16][is.na(itemanalyse[,12:16])] <- " "
-}
 
 ## Voeg gebruikte sleutel toe aan itemanalyse
 tsleutel <- as.data.frame(t(sleutel))
