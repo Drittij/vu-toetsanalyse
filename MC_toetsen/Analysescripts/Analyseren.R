09cdba3c07552867f3f2a2f4a4c48d465bb5bb74
Daniel Drittij <d.drittij@vu.nl>
2018-08-09 13:33:38 +0200

    Analysestap bij tentamens met meerdere versies samengevoegd ivm efficientie onderhoud.

--- MC_toetsen/Analysescripts/Analyseren.R
+++ MC_toetsen/Analysescripts/Analyseren.R
@@ -5,14 +5,6 @@ if (nrv == 1) {
   ll <- parse(file = "MC_toetsen/Analysescripts/Heranalyse Toetsanalyse 1 versie stap 2.R")
 }
 
-if (nrv == 2) {
-  ll <- parse(file = "MC_toetsen/Analysescripts/Heranalyse Toetsanalyse 2 versies stap 2.R") 
-}
-
-if (nrv == 3) {
-  ll <- parse(file = "MC_toetsen/Analysescripts/Heranalyse Toetsanalyse 3 versies stap 2.R") 
-}
-
-if (nrv == 4) {
-  ll <- parse(file = "MC_toetsen/Analysescripts/Heranalyse Toetsanalyse 4 versies stap 2.R") 
+if (nrv > 1) {
+  ll <- parse(file = "MC_toetsen/Analysescripts/Heranalyse Toetsanalyse meerdere versies stap 2.R") 
 }
